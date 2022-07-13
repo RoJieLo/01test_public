bdb3c94832e67c32081da41ca24118c2fd8249e7
RoJieLo <108521371+RoJieLo@users.noreply.github.com>
2022-07-13 16:45:42 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+
+<html>
+<head>
+<title>GanGutech</title>
+</head>
+<body>
+  <h1>
+  帳號登入
+  </h1>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+      <form action="login_put.php" method="post">
+      <p>請輸入帳號：<input type="text" name="account" autofocus /></p>
+      <p>請輸入密碼：<input type="password" name="password"  /></p>
+      <p><input type="submit" name="button" value="登入" /></p>
+      </form>
+</body>
+</html>
+
+
